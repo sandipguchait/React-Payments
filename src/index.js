6b7fef8032e6b9da81b4aef0a7395878cb6b2f69
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-08 19:32:43 +0530

    Completing the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./styles.css";
 
+toast.configure();
+
 function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 64998.67,
+    description: "Cool car"
+  });
+
+  async function handleToken(token) {
+    const response = await axios.post(
+      //codesandbox made node server
+      "https://yv1xp997q9.sse.codesandbox.io/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_QREVHSBFyM21NjYBhGpnef9U"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
     </div>
   );
 }
